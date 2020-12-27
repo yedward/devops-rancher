d3442ff32b1eaac72ef8b1eb5b76c00efdef6556
yedward <yedward92@qq.com>
2020-12-27 23:25:30 +0800

    modify

--- src/devops/Rancher.groovy
+++ src/devops/Rancher.groovy
@@ -1,16 +1,15 @@
-package devops;
+package devops
 
 def name = "zhangsan"
 def age = 25
 
-def deploy() {
+def deploy = {
     println("发布成功 ${name}")
     System.out.println("system 发布成功 ${age}")
 }
 
-def testHttpRequest(param1) {
+def testHttpRequest = { param1 ->
     def response = httpRequest "http://httpbin.org/response-headers?param1=${param1}"
     println('Status: '+response.status)
     println('Response: '+response.content)
 }
-
