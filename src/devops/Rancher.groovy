a69807f8085fb66be9c8b4de933ec38f52e29d85
yedward <yedward92@qq.com>
2020-12-27 22:36:28 +0800

    modify

--- src/devops/Rancher.groovy
+++ src/devops/Rancher.groovy
@@ -4,9 +4,22 @@ package devops;
  * Rancher Tools
  */
 class Rancher {
+    /**
+     * 发布
+     * @return
+     */
     def deploy() {
         println("发布成功")
         System.out.println("system 发布成功")
+    }
+
+    /**
+     * 测试HTTP请求
+     */
+    def testHttpRequest(param1) {
+        def response = httpRequest "http://httpbin.org/response-headers?param1=${param1}"
+        println('Status: '+response.status)
+        println('Response: '+response.content)
     }
 }
 
