77a645ec551984c30defb811ee23173a923f86d0
yedward <yedward92@qq.com>
2020-12-29 00:10:00 +0800

    modify

--- vars/rancher.groovy
+++ vars/rancher.groovy
@@ -2,3 +2,6 @@ def info() {
     println("rancher info .............")
 }
 
+def deployApp(String url, String namespace) {
+    println("deploy app from url:${url} to namespace:${namespace} success.")
+}
