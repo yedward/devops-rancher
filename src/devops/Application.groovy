ecba0cff63e8c544ffbc987f98ac1bac6f7bb29f
yedward <yedward92@qq.com>
2021-01-06 03:22:04 +0800

    modfiy

--- src/devops/Application.groovy
+++ src/devops/Application.groovy
@@ -18,6 +18,7 @@ def handleRequest(String httpMode, String httpUrl, String requestBody) {
             ignoreSslErrors: true,
             validResponseCodes: "100:599",
             quiet: false,
+            responseHandle: "STRING",
             requestBody: "${requestBody}",
             url: "${ApplicationConfig.RANCHER_SERVER_INFO.API_SERVER_URL}/${httpUrl}"
     println("reponse::" + response)
