5f20346414d57beb8d58dd1c59f581a6e2a202aa
yedward <yedward92@qq.com>
2021-01-07 11:53:28 +0800

    modify

--- src/devops/RancherApps.groovy
+++ src/devops/RancherApps.groovy
@@ -1,39 +1,24 @@
 package devops
 
 /**
- * List apps
- */
-def ls() {
-    def url = "sdfdsfsdf/sdfsf"
-    println("List apps")
-}
-
-/**
- * Delete an app
- */
-def delete() {
-    println("Delete an app")
-}
-
-/**
  * 从应用商店部署应用
  *
  * 示例：
  *  url: https://rancher_api_url/v3/projects/your_cluster_project_id/app
  *  method:POST
  *  request payload：
-// {
-// "prune": false,
-// "timeout": 300,
-// "wait": false,
-// "type": "app",
-// "name": "your_app_release_name",
-// "answers": {},
-// "targetNamespace": "your_namespace",
-// "externalId": "catalog://?catalog=your_project_id/your_catalog_name&type=projectCatalog&template=your_catalog_app_name&version=your_catalog_app_version",
-// "projectId": "your_cluster_project_id",
-// "valuesYaml": "---{}\n"
-// }
+ *  {
+ *  "prune": false,
+ *  "timeout": 300,
+ *  "wait": false,
+ *  "type": "app",
+ *  "name": "your_release_name",
+ *  "answers": {},
+ *  "targetNamespace": "your_namespace",
+ *  "externalId": "catalog://?catalog=your_project_id/your_catalog_name&type=projectCatalog&template=your_catalog_app_name&version=your_catalog_app_version",
+ *  "projectId": "your_cluster_project_id",
+ *  "valuesYaml": "your_values_yaml"
+ *  }
  */
 def install(String clusterProjectId, String projectId, String releaseName, String namespaceName, String catalogName, String catalogAppName, String catalogAppVersion, String valuesYaml) {
     println("---------------【从应用商店部署应用】开始--------------")
@@ -58,15 +43,31 @@ def install(String clusterProjectId, String projectId, String releaseName, Strin
 }
 
 /**
- * Rollback an app to a previous version
+ * 从应用商店中升级已部署的应用
+ *
+ * 示例：
+ *  url: https://rancher_api_url/v3/project/your_cluster_project_id/apps/your_project_id:your_release_name?action=upgrade
+ *  method:POST
+ *  request payload：
+ *  {
+ *  "externalId": "catalog://?catalog=your_project_id/your_catalog_name&type=projectCatalog&template=your_catalog_app_name&version=your_catalog_app_version",
+ *  "answers": {},
+ *  "valuesYaml": "your_values_yaml",
+ *  "forceUpgrade": false
+ *  }
  */
-def rollback() {
-    println("Rollback an app to a previous version")
-}
-
-/**
- * Upgrade an existing app to a newer version
- */
-def upgrade() {
-    println("Upgrade an existing app to a newer version")
+def upgrade(String clusterProjectId, String projectId, String releaseName, String catalogName, String catalogAppName, String catalogAppVersion, String valuesYaml) {
+    println("---------------【从应用商店中升级已部署的应用】开始--------------")
+    Application application = new Application()
+    String requestBody = """
+    {
+    "externalId": "catalog://?catalog=${projectId}/${catalogName}&type=projectCatalog&template=${catalogAppName}&version=${catalogAppVersion}",
+    "answers": {},
+    "valuesYaml": "${valuesYaml}",
+    "forceUpgrade": false
+    }
+"""
+    def response = application.handleRequest("POST","/project/${clusterProjectId}/apps/${projectId}:${releaseName}?action=upgrade", "${requestBody}")
+    println("---------------【从应用商店中升级已部署的应用】结束--------------")
+    return response
 }
